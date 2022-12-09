fd38ebdcf39c43ac1ce2713dcebf85f916f37694
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2022-12-09 16:03:23 +0900

    first commit

---
+++ my-app/pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next練習中</title>
+        <meta name="description" content="Next練習中" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Next練習中
+        </h1>
+
+        <h2>２回目はVercleにデプロイできるのか</h2>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://borderlesss.net/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >https://borderlesss.net/
+        </a>
+      </footer>
+    </div>
+  )
+}
